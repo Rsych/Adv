1f4ac406e8b07268877d1394f6eb457faf46aa93
Ryan <tamielryan@gmail.com>
2022-02-03 17:10:10 +0200

    Initial Commit

---
+++ Adv/AdvApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvApp.swift
+//  Adv
+//
+//  Created by Ryan J. W. Kim on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct AdvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
