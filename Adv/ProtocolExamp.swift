2158a418c16ba96c7dad968360083e7d269db488
Ryan <tamielryan@gmail.com>
2022-03-20 23:06:49 +0900

    Protocol

--- Adv/ProtocolExamp.swift
+++ Adv/ProtocolExamp.swift
@@ -7,39 +7,83 @@
 
 import SwiftUI
 
-struct DefaultColorTheme {
+struct DefaultColorTheme: ColorThemeProtocol {
     let primary: Color = .blue
     let secondary: Color = .white
     let tertiary: Color = .gray
 }
 
-struct AlternativeColorTheme {
+struct AlternativeColorTheme: ColorThemeProtocol {
     let primary: Color = .red
     let secondary: Color = .white
     let tertiary: Color = .green
 }
 
+struct AnotherColorTheme: ColorThemeProtocol {
+    var primary: Color = .blue
+    var secondary: Color = .red
+    var tertiary: Color = .purple
+    
+    
+}
+
+protocol ColorThemeProtocol {
+    var primary: Color { get }
+    var secondary: Color { get }
+    var tertiary: Color { get }
+}
+
+protocol ButtonTextProtocol {
+    var buttonText: String { get }
+    
+}
+
+protocol ButtonPressedProtocol {
+    func buttonPressed()
+}
+
+protocol ButtonDataSourceProtocol: ButtonTextProtocol, ButtonPressedProtocol {
+    
+}
+
+class DefaultDataSource: ButtonDataSourceProtocol {
+    var buttonText: String = "Protocols"
+    
+    func buttonPressed() {
+        print("Button was pressed!")
+    }
+}
+
+class AlternativeDataSource: ButtonTextProtocol {
+    var buttonText: String = "Protocols is bad?"
+}
+
 struct ProtocolExamp: View {
 //    let colorTheme: DefaultColorTheme = DefaultColorTheme()
-    let colorTheme: AlternativeColorTheme = AlternativeColorTheme()
+//    let colorTheme: AlternativeColorTheme = AlternativeColorTheme()
+    let colorTheme: ColorThemeProtocol
+    let dataSource: ButtonDataSourceProtocol
     
     var body: some View {
         ZStack {
             colorTheme.tertiary
                 .ignoresSafeArea()
             
-            Text("Protocols")
+            Text(dataSource.buttonText)
                 .font(.headline)
                 .foregroundColor(colorTheme.secondary)
                 .padding()
                 .background(colorTheme.primary)
                 .cornerRadius(10)
+                .onTapGesture {
+                    dataSource.buttonPressed()
+                }
         }
     }
 }
 
 struct ProtocolExamp_Previews: PreviewProvider {
     static var previews: some View {
-        ProtocolExamp()
+        ProtocolExamp(colorTheme: DefaultColorTheme(), dataSource: DefaultDataSource())
     }
 }
