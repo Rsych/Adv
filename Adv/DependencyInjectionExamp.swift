20651c0cafb3d8b556ef9e7c5cca853427f84c95
Ryan <tamielryan@gmail.com>
2022-03-21 21:03:15 +0900

    DepInjection

---
+++ Adv/DependencyInjectionExamp.swift
@@ -0,0 +1,20 @@
+//
+//  DependencyInjectionExamp.swift
+//  Adv
+//
+//  Created by Ryan J. W. Kim on 2022/03/20.
+//
+
+import SwiftUI
+
+struct DependencyInjectionExamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DependencyInjectionExamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DependencyInjectionExamp()
+    }
+}
