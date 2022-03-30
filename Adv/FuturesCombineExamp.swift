9c139e471ae3cdea5ca933d030170a1051e355cf
Ryan <tamielryan@gmail.com>
2022-03-30 23:00:45 +0900

    Futures

---
+++ Adv/FuturesCombineExamp.swift
@@ -0,0 +1,20 @@
+//
+//  FuturesCombineExamp.swift
+//  Adv
+//
+//  Created by Ryan J. W. Kim on 2022/03/29.
+//
+
+import SwiftUI
+
+struct FuturesCombineExamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FuturesCombineExamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FuturesCombineExamp()
+    }
+}
