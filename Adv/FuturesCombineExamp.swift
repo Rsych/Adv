77a0b5384c0ae34908a4ed63722a29a368a99b06
Ryan <tamielryan@gmail.com>
2022-04-01 15:35:42 +0300

    Combine, Escaping closure

--- Adv/FuturesCombineExamp.swift
+++ Adv/FuturesCombineExamp.swift
@@ -6,10 +6,56 @@
 //
 
 import SwiftUI
+import Combine
+
+// Download with combine
+// Download with @escaping closure
+// convert @escaping closure to combine
+
+class FuturesCombineViewModel: ObservableObject {
+    @Published var title: String = "Starting title"
+    let url = URL(string: "https://www.google.com")!
+    var cancellable = Set<AnyCancellable>()
+    
+    init() {
+        download()
+    }
+    
+    func download() {
+//        getCombinePublisher()
+//            .sink { _ in
+//
+//            } receiveValue: { [weak self] returnedValue in
+//                self?.title = returnedValue
+//            }
+//            .store(in: &cancellable)
+        
+        getEscapingClosure { [weak self] returnedValue, error in
+            self?.title = returnedValue
+        }
+    }
+    
+    func getCombinePublisher() -> AnyPublisher<String, URLError> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .timeout(1, scheduler: DispatchQueue.main)
+            .map({ _ in
+                return "New value"
+            })
+            .eraseToAnyPublisher()
+    }
+    
+    func getEscapingClosure(completionHandler: @escaping (_ value: String, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            completionHandler("New value 2", nil)
+        }
+        .resume()
+    }
+}
 
 struct FuturesCombineExamp: View {
+    @StateObject private var vm = FuturesCombineViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(vm.title)
     }
 }
 
